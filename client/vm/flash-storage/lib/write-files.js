46e82d9eeff7058f76bd6f0b24647e4618fca3be
Chris Troutner <chris.troutner@gmail.com>
2018-01-06 08:31:40 -0800

    Adding sudo package to vm flash client

--- client/vm/flash-storage/lib/write-files.js
+++ client/vm/flash-storage/lib/write-files.js
@@ -43,6 +43,7 @@ RUN curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 RUN bash nodesource_setup.sh
 RUN apt-get install -y nodejs
 RUN apt-get install -y build-essential
+RUN apt-get install -y sudo
 WORKDIR /root
 VOLUME /usr/src/app/logs
 COPY package.json package.json
