49d49b9f8c144177cef3871b20ebe551efc24fe4
Chris Troutner <chris.troutner@gmail.com>
2018-01-07 08:13:38 -0800

    Fixing bugs from the refactor

--- client/vm/simple/p2p-vps-client.js
+++ client/vm/simple/p2p-vps-client.js
@@ -32,13 +32,11 @@
 
 // Express Dependencies
 const express = require("express");
-const getStream = require("get-stream");
 const execa = require("execa");
 
 // Global Variables
 const app = express();
 const port = 4000;
-let checkExpirationTimer;
 
 // Read in device-config.json file
 let deviceConfig;
@@ -160,7 +158,9 @@ function registerDevice() {
       logr.log("Docker image has been built and is running.");
 
       // Begin timer to check expiration.
-      p2pVpsServer.startExpirationTimer();
+      //p2pVpsServer.startExpirationTimer();
+      console.log(`p2p-vps-client.js/registerDevice: ${typeof registerDevice}`);
+      p2pVpsServer.startExpirationTimer(registerDevice);
     })
 
     .catch(err => {
